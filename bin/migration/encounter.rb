4bc087418dd8e0505bf68033b21a9a7ff1185baa
Pachawo Bisani <phbisan@gmail.com>
2017-02-13 15:31:56 +0200

    Modifed encounter migration script

--- bin/migration/encounter.rb
+++ bin/migration/encounter.rb
@@ -16,7 +16,20 @@ $concept_map = {
     "HTC Test 1 Time frame" => 'First Pass Test Kit 1 Testing Duration (Minutes)',
     "HTC Test 2 Time frame" => 'First Pass Test Kit 2 Testing Duration (Minutes)',
     "Result of HIV test" => 'Result Given to Client',
-    "test 1 lot number" => 'First Pass Test Kit 1 Lot Number'
+    "test 1 lot number" => 'First Pass Test Kit 1 Lot Number',
+    "Routine HTC with health service" => 'Routine HTS (PITC) within Health Service',
+    "Comes with HTC family Ref slip" => 'Comes with HTS Family Reference Slip',
+    "Never Tested" => 'Never Tested',
+    "Last Negative" => 'Last Negative',
+    "Last Positive" => 'Last Positive',
+    "Last Inconclusive" => 'Last Inconclusive',
+    "Yes" => 'Yes',
+    "No" => 'No',
+    "Other" => "Other",
+    "Reactive" => 'Reactive',
+    "Non-reactive" => 'Non-reactive',
+    "Low risk" => 'Low risk',
+    "AVD+ or High Risk" => "High risk"
   }
 
 $encounter_map = {
@@ -50,7 +63,7 @@ def start
 
   `cd #{File_destination} && touch observations.sql encounters.sql`
   `echo -n '#{obs_sql_statement}' >> #{File_destination}/observations.sql`
-  `echo -n '#{obs_sql_statement}' >> #{File_destination}/observations.sql`
+  `echo -n '#{encounter_sql_statement}' >> #{File_destination}/encounters.sql`
 
   encounters = Encounter.all
 
@@ -64,7 +77,7 @@ def start
     enc_datetime = encounter['encounter_datetime']
     enc_creator = encounter['creator']
     enc_date_created = encounter['date_created']
-    enc_voided = encounter['voided']
+    enc_voided = encounter['voided'] ? 1 : 0
     enc_voided_by = encounter['voided_by']
     enc_date_voided = encounter['date_voided']
     enc_void_reason = encounter['void_reason']
@@ -91,10 +104,30 @@ EOF
       accession_number = ob['accession_number']
       value_group_id = ob['value_group_id']
       value_boolean = ob['value_boolean']
+      value_coded_name_id = ob['value_coded_name_id']
+      value_drug = ob['value_drug']
+      value_datetime = ob['value_datetime']
+      value_numeric = ob['value_numeric']
+      value_modifier = ob['value_modifier']
+      value_text = ob['value_text']
+      value_complex = ob['value_complex']
+      comments = ob['comments']
+      creator = ob['creator']
+      date_created = ob['date_created']
+      voided = ob['voided'] ? 1 : 0
+      voided_by = ob['voided_by']
+      date_voided = ob['date_voided']
+      value_coded = ob['value_coded']
 
-      concept_name = ConceptName.find_by_concept_id(ob['concept_id']).name
+      if !value_coded.blank?
+        old_value_name = ConceptName.find_by_concept_id(value_coded).name rescue nil
+        new_value_name = $concept_map[old_value_name]
+        puts "pppppppppppppppppppppppppppppppppppppp#{new_value_name}"
+        value_coded = HtsConceptName.find_by_name(new_value_name).concept_id rescue nil
+      end
+
+      concept_name = ConceptName.find_by_concept_id(ob['concept_id']).name rescue nil
       new_concept_name = $concept_map[concept_name]
-      puts "#{new_concept_name}"
       concept_id = HtsConceptName.find_by_name(new_concept_name).concept_id rescue nil
       
       if $encounter_map["PRE TEST COUNSELLING"].include? new_concept_name
@@ -106,21 +139,6 @@ EOF
       elsif $encounter_map["HIV TESTING"].include? new_concept_name
         enc_type = HtsEncounterType.find_by_name("HIV TESTING").encounter_type_id
       end
-
-      value_coded = ob['value_coded']
-      value_coded_name_id = ob['value_coded_name_id']
-      value_drug = ob['value_drug']
-      value_datetime = ob['value_datetime']
-      value_numeric = ob['value_numeric']
-      value_modifier = ob['value_modifier']
-      value_text = ob['value_text']
-      value_complex = ob['value_complex']
-      comments = ob['comments']
-      creator = ob['creator']
-      date_created = ob['date_created']
-      voided = ob['voided']
-      voided_by = ob['voided_by']
-      date_voided = ob['date_voided']
 
       uuid = ActiveRecord::Base.connection.select_one <<EOF
             select uuid()
@@ -136,13 +154,12 @@ EOF
       `echo -n '#{obs_sql}' >> #{File_destination}/observations.sql`
     end
 
-    encounter_type = HtsEncounterType.find_by_encounter_type_id(enc_type).name rescue nil
-    puts "{{{{{{{{{{{{{{#{encounter_type}}}}}}}}}}}}}}}}}}}}"
-
     enc_sql_statement =  "(\"#{enc_id}\",\"#{enc_type}\",\"#{enc_patient_id}\",\"#{provider_id}\",\"#{enc_location_id}\","
     enc_sql_statement += "\"#{enc_form_id}\",\"#{enc_datetime}\",\"#{enc_creator}\",\"#{enc_date_created}\",\"#{enc_voided}\","
     enc_sql_statement += "\"#{enc_voided_by}\",\"#{enc_date_voided}\",\"#{enc_void_reason}\",\"#{uuid.values.first}\","
     enc_sql_statement += "\"#{enc_changed_by}\",\"#{enc_date_changed}\",\"#{patient_program_id = 1}\"),"
+
+    `echo -n '#{enc_sql_statement}' >> #{File_destination}/encounters.sql`
 
   end
 
